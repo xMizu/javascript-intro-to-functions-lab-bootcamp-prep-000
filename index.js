1c6fdf88522b69cc50d5d4808db2bf833f9df148
Anthony Wan <anthonywan00@gmail.com>
2019-04-07 23:38:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ function logWhisper(string)
 
 function  sayHiToGrandma(string)
 {
-  if string === string.toLowerCase()
+  if (string === string.toLowerCase())
     return "I can't hear you!"
 }
 
