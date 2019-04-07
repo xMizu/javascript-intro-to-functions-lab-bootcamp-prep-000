d7c060e0a6389187771d1f29147b6a24079fa6ee
Anthony Wan <anthonywan00@gmail.com>
2019-04-07 23:45:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,8 +21,10 @@ function logWhisper(string)
 function  sayHiToGrandma(string)
 {
   if (string === string.toLowerCase())
-    return "I can't hear you!"
-    else if (string === string.toUpperCase())
+    {
+      return "I can't hear you!"
+      }
+      else if (string === string.toUpperCase())
       return "YES INDEED!"
     else
       
