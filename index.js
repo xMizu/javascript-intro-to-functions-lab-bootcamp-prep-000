d47dc6c99d46439690449e33bc3511dce5e2c4e2
Anthony Wan <anthonywan00@gmail.com>
2019-04-07 23:23:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,5 +10,10 @@ function whisper(string)
 
 function logShout(string)
 {
-  console.log
+  console.log(string.toUpperCase)
+}
+
+function logWhisper(string)
+{
+  console.log(string.toUpperCase)
 }
